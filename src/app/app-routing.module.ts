1545f5a1ec4ab4c7038016d13f3cf26ea7148d5a
Abhiavhad30 <abhijitavhad727@gmail.com>
2026-03-13 11:56:40 +0530

    website for real estate company

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component: HomeComponent},
+  {path: 'about',component: AboutComponent},
+  {path: 'services',component: ServicesComponent},
+  {path: 'contact',component: ContactComponent},
+//   { path: '', redirectTo: '/', pathMatch: 'full' } // default route
+
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
